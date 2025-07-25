af4cb73ffcb09ba35d5d3e2930392bf129f987d2
ahmadfauzi2005 <130649613+ahmadfauzi2005@users.noreply.github.com>
2025-07-25 07:11:08 +0700

    Update shop.js

--- shop.js
+++ shop.js
@@ -41,7 +41,7 @@ async function loadShopProducts() {
             <a href="shop-details.html">${name}</a>
           </div>
           <div class="text-sm text-gray-500 mb-1">${category}</div>
-          <div class="product-price text-indigo-700 font-bold">Rp${p.price.toLocaleString('id-ID')}</div>
+          <div class="product-price text-indigo-700 font-bold">Rp${price.toLocaleString('id-ID')}</div>
         </div>
       </div>`;
     });
